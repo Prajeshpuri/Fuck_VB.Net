3583a63d650bb6ea9593f9656a3dc43376313786
Prajeshpuri <prajeshpuri@gmail.com>
2018-10-23 15:27:50 +0530

    fuckoff_vb.net

---
+++ gui2/gui2/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    Dim x, y As Integer
+    Dim o As Char
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text + Button1.Text
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text + Button2.Text
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text + Button3.Text
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text + Button4.Text
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text + Button6.Text
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        TextBox1.Text = TextBox1.Text + Button8.Text
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text + Button5.Text
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text + Button7.Text
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        TextBox1.Text += Button9.Text
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        TextBox1.Text = TextBox1.Text + Button15.Text
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        x = CInt(TextBox1.Text)
+        TextBox1.Text = ""
+        o = Button10.Text
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        x = CInt(TextBox1.Text)
+        TextBox1.Clear()
+        o = Button11.Text
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        x = CInt(TextBox1.Text)
+        TextBox1.Text = ""
+        o = Button12.Text
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        x = CInt(TextBox1.Text)
+        TextBox1.Text = ""
+        o = Button13.Text
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        If TextBox1.Text <> "" Then
+            y = CInt(TextBox1.Text)
+            TextBox1.Text = ""
+            Select Case o
+                Case "+"
+                    TextBox1.Text = x + y
+                Case "-"
+                    TextBox1.Text = x - y
+                Case "*"
+                    TextBox1.Text = x * y
+                Case "/"
+                    TextBox1.Text = x / y
+            End Select
+        End If
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        TextBox1.Clear()
+    End Sub
+End Class
+
